e19eadbfeee6c46b0e84e647af229a2186cc7f5d
42doji <doji@student.42gyeongsan.kr>
2024-05-27 00:58:06 +0900

    setup

--- main.c
+++ main.c
@@ -2,19 +2,15 @@
 
 int	init_table(t_table *rules, int argc, char *argv[])
 {
+	if (!rules)
+		return (0);
 	rules->num_philosophers = ft_atoi(argv[1]);
 	if (!rules->num_philosophers)
 		return (0);
 	rules->num_forks = rules->num_philosophers;
 	rules->time_to_die = ft_atoi(argv[2]);
-	if (!rules->time_to_die)
-		return (0);
 	rules->time_to_eat = ft_atoi(argv[3]);
-	if (!rules->time_to_eat)
-		return (0);
 	rules->time_to_sleep = ft_atoi(argv[4]);
-	if (!rules->time_to_sleep)
-		return (0);
 	if (argc == 6)
 		rules->num_times_to_eat = ft_atoi(argv[5]);
 	else
@@ -35,7 +31,16 @@ void	free_philosophers(t_philo **philosophers, int i)
 	free(philosophers);
 }
 
-t_philo	**create_philosophers(t_table *rules)
+void	*philo_callback(void *arg)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)arg;
+	ft_printf("Philosopher %d is alive\n", philo->philo_id);
+	return (NULL);
+}
+
+t_philo	**create_philosophers(t_table *rules, pthread_attr_t *attr)
 {
 	t_philo	**philosophers;
 	int		i;
@@ -54,8 +59,7 @@ t_philo	**create_philosophers(t_table *rules)
 		}
 		philosophers[i]->philo_id = i + 1;
 		philosophers[i]->eat_count = 0;
-		philosophers[i]->thread_handle = NULL;
-		philosophers[i]->thread_attr = NULL;
+		pthread_create(philosophers[i]->thread_handle, NULL, philo_callback, philosophers[i]);
 		i++;
 	}
 	return (philosophers);
@@ -107,23 +111,22 @@ t_fork	**create_forks(int n)
 
 int main(int argc, char *argv[])
 {
-	t_philo	**philosophers;
-	t_fork	**forks;
-	t_table rules;
-	
+	t_philo			**philosophers;
+	t_fork			**forks;
+	t_table 		rules;
+	pthread_attr_t	attr;
+
 	if (argc < 5 || argc > 6)
 		return (1);
 	if(!init_table(&rules, argc, argv))
 		return (1);
-	philosophers = create_philosophers(&rules);
-	if (!philosophers)
-		return (1);
 	forks = create_forks(rules.num_forks);
 	if (!forks)
-	{
-		free_philosophers(philosophers, rules.num_philosophers - 1);
 		return (1);
-	}
+	philosophers = create_philosophers(&rules, &attr);
+	if (!philosophers)
+		return (1);
+
 	free_forks(forks, rules.num_forks - 1);
 	return (0);
 }
